7d5957a27a65b1a7e78f0959f586cefcb795dd60
Abd Abd <thefitnessfab@gmail.com>
2020-04-18 17:21:29 +0100

    added redux and implemented userReducer

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FromInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../firebase/firebase.utils';
 class SignIn extends React.Component {
     constructor(props){
         super(props);
@@ -12,9 +12,16 @@ class SignIn extends React.Component {
             password:''
         }
     }
-    handelSubmit = event =>{
+    handelSubmit = async event =>{
         event.preventDefault();
-        this.setState({email:'',password:''});
+        const {email, password} = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email:'',password:''});
+
+        } catch(error){
+            console.log(error);
+        }
     }
     handelChange = event => {
         const {name, value} = event.target;
